e1f3231c595e1ff456f321d75cba5d464cbc0d4b
shmech <shmech@PGX4966.pgx.local>
2014-06-19 09:20:26 -0600

    Testing Commits Push/Pull

--- lib/differences.rb
+++ lib/differences.rb
@@ -1,6 +1,7 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+# This was fun
 
 def check_usage
   unless ARGV.length == 2
